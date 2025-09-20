20ad84418825c48e522a50c5af485e843eae6021
Steven Reyher <stevenreyher@gmail.com>
2025-09-20 15:30:26 -0400

    Initial Unreal project setup for Cabin Horror Game

---
+++ Source/NoahDad/NoahDad.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NoahDad.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NoahDad, "NoahDad" );
